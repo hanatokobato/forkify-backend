e4134d047e4c1fa18132192ce35fa3b25713e4ae
bangld <kobato2096@gmail.com>
2022-03-27 19:03:20 +0700

    Add docker

---
+++ docker/docker-entrypoint.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Exit on fail
+set -e
+
+# Bundle install
+bundle install --jobs=4
+
+yarn install --check-files
+
+# Waiting for dependent containers
+/wait-for-it.sh -c 'nc -z psql 5432'
+
+# Migrate
+bundle exec rake db:create
+bundle exec rake db:migrate
+
+# Remove puma pid if existed
+rm -f tmp/pids/server.pid
+
+# Start services
+bundle exec rails server --port=3000 -b 0.0.0.0
+
+# Finally call command issued to the docker service
+exec "$@"
