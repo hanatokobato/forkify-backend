9f1de86c796b684701b2c33d06f3cf5d5111dcb4
bangld <kobato2096@gmail.com>
2023-05-11 13:46:12 +0700

    Add health check

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   if Rails.env.development?
     mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
   end
+  get "/health_check", to: "health_check#show"
   post "/graphql", to: "graphql#execute"
 end
