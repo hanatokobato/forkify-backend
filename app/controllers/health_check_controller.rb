88781d3f5c09c592ab12f25dc85112ed8424d70d
bangld <kobato2096@gmail.com>
2023-05-11 14:13:13 +0700

    Fix no template

--- app/controllers/health_check_controller.rb
+++ app/controllers/health_check_controller.rb
@@ -1,8 +1,5 @@
 class HealthCheckController < ApplicationController
-  rescue_from(Exception) { render head: 503 }
-
   def show
-    format.html { render head: 200 }
-    format.json { render head: 200 }
+    head :ok
   end
 end
